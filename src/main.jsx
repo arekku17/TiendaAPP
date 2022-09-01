94379fc1de580f5e00381101e924c5ea3730e50d
Arekku <jose_alex170702@hotmail.com>
2022-09-01 18:27:24 -0500

    add _redirects in public to resolve netlify error 404

--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import Products from './components/Products'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import Products from "./components/Products";
+import "./index.css";
 import  {
-  BrowserRouter,
-  Routes,
-  Route,
-  Navigate
+    BrowserRouter,
+    Routes,
+    Route,
+    Navigate
 } from "react-router-dom";
-import SignIn from './components/SignIn'
-import Signup from './components/Signup'
+import SignIn from "./components/SignIn";
+import Signup from "./components/Signup";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Navigate to="/home" />}/>
-        <Route path='/home' element={<App/>} />
-        <Route path='/product/:id' element={<Products/>} /> {/**cambiar nombre de ruta */}
-        {/*<Route path='/products' element={<Products/>} />*/}
-        <Route path='/signin' element={<SignIn/>} />
-        <Route path='/signup' element={<Signup/>} />
-        <Route path='*' element={<h3>Error 404</h3>} />
-      </Routes>
-    </BrowserRouter>    
-  </React.StrictMode>
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Navigate to="/home" />}/>
+                <Route path='/home' element={<App/>} />
+                <Route path='/product/:id' element={<Products/>} /> {/**cambiar nombre de ruta */}
+                {/*<Route path='/products' element={<Products/>} />*/}
+                <Route path='/signin' element={<SignIn/>} />
+                <Route path='/signup' element={<Signup/>} />
+                <Route path='*' element={<h3>Error 404</h3>} />
+            </Routes>
+        </BrowserRouter>    
+    </React.StrictMode>
+);
 
